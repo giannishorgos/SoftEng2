25c0a3d1b5ed104c051b2d7f8261670f3e940c97
conmylo <k.mylonas@ysoft.gr>
2023-12-08 14:43:34 +0100

    Default commit message

--- tests/init.test.js
+++ tests/init.test.js
@@ -5,24 +5,24 @@ test.before(t => {
     t.context.expected = {
         'application/json': {
             "goalID": 0,
-            "goalAmount": 1.4658129,
-            "goalMonths": 5,
+            "amount": 1.4658129,
+            "months": 5,
             "userID": 6
         }
     };
 })
 
 test('getGoalByID test', async t => {
-    const promise = getGoalByID('test_user_id', 'test_goal_id')
+    const promise = await getGoalByID('test_user_id', 'test_goal_id')
     t.deepEqual(promise, t.context.expected['application/json'])
 })
 
 test('getGoals test', async t => {
-    const promise = getGoals('test_user_id')
+    const promise = await getGoals('test_user_id')
     t.deepEqual(promise, Array(2).fill(t.context.expected['application/json']))
 })
 
 test('putGoal test', async t => {
-    const promise = putGoal('test_body', 'test_user_id', 'test_goal_id')
+    const promise = await putGoal('test_body', 'test_user_id', 'test_goal_id')
     t.deepEqual(promise, t.context.expected['application/json'])
 })
