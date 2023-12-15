5826c504c661fb028bf71904ff6afbaf1db385ca
Bobotsalos <xaris.kem@gmail.com>
2023-12-15 13:34:52 +0200

    add personal info endpoint test

--- tests/personalInfo.spec.js
+++ tests/personalInfo.spec.js
@@ -1,4 +1,5 @@
 const test = require('ava')
+const { got } = require('got-cjs')
 const { getPersonalInfo, postPersonalInfo, putPersonalInfo } = require('../service/PersonalInfoService')
 
 test.before(t => {
@@ -12,6 +13,15 @@ test.before(t => {
             "username" : 'username',
         }
     };
+    t.context.endpoint_expected = {
+        userID: 1,
+        username: 'admin',
+        fullname: 'John Doe',
+        password: 'password',
+        email: 'johnDoe@example.com',
+        age: 70
+    }
+    t.context.prefixUrl = "https://virtserver.swaggerhub.com/KMYLONAS_1/SoftwareEngineering1/1.0.0/"
 })
 
 test('getPersonalInfo test', async t => {
@@ -29,4 +39,30 @@ test('putPersonalInfo test', async t => {
     const promise = await putPersonalInfo('test_body', 'test_user_id')
     t.deepEqual(promise, t.context.expected['application/json'])
 
+})
+
+test('GET Personal Info test', async t => {
+    const response = await got('user/1/personalinfo', {
+        prefixUrl: t.context.prefixUrl
+    })
+    t.deepEqual(JSON.parse(response.body), t.context.endpoint_expected)
+    t.is(response.statusCode, 200)
+})
+
+test('POST Personal Info test', async t => {
+    const response = await got.post('user/personalinfo', {
+        prefixUrl: t.context.prefixUrl,
+        json: t.context.endpoint_expected
+    })
+    t.deepEqual(JSON.parse(response.body), t.context.endpoint_expected)
+    t.is(response.statusCode, 201)
+})
+
+test('PUT Personal Info test', async t => {
+    const response = await got.put('user/1/personalinfo', {
+        prefixUrl: t.context.prefixUrl,
+        json: t.context.endpoint_expected
+    })
+    t.deepEqual(JSON.parse(response.body), t.context.endpoint_expected)
+    t.is(response.statusCode, 200)
 })
